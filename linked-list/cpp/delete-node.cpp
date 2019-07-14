6f0c00980ad924e0c3bf727673264ce92c541665
Shruti Gandhi <shruti4gandhi@gmail.com>
2019-07-13 18:04:23 -0700

    LC - Delete Node in a Linked List

---
+++ linked-list/cpp/delete-node.cpp
@@ -0,0 +1,21 @@
+/* Delete a node (except the tail) in a singly linked list, given only access to
+   that node.
+   LC 237. Delete Node in a Linked List
+   Author: Shruti
+*/
+
+//Definition for singly-linked list.
+struct ListNode {
+  int val;
+  ListNode *next;
+  ListNode(int x) : val(x), next(NULL) {}
+};
+
+class Solution {
+public:
+    void deleteNode(ListNode* node) {
+        ListNode* temp = node->next;
+        node->val = temp->val;
+        node->next = temp->next;
+    }
+};
