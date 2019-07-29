33492f286ce9fb8a1d3139d51d6772b26edd5f9e
Shruti Gandhi <sgandhi3@ncsu.edu>
2019-07-29 14:07:52 -0700

    LC - Maximum Depth of N-ary Tree

--- trees/cpp/maximum-depth-of-a-n-ary-tree.cpp
+++ trees/cpp/maximum-depth-of-a-n-ary-tree.cpp
@@ -5,15 +5,21 @@
    Author: Shruti
 */
 
-/**
- * Definition for a binary tree node.
- * struct TreeNode {
- *     int val;
- *     TreeNode *left;
- *     TreeNode *right;
- *     TreeNode(int x) : val(x), left(NULL), right(NULL) {}
- * };
- */
+/*
+// Definition for a Node.
+class Node {
+public:
+    int val;
+    vector<Node*> children;
+
+    Node() {}
+
+    Node(int _val, vector<Node*> _children) {
+        val = _val;
+        children = _children;
+    }
+};
+*/
 
 class Solution {
 public:
