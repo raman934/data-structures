7592499a22113c5354394a2dd604b66165c5047b
Shruti Gandhi <sgandhi3@ncsu.edu>
2019-08-06 21:00:55 -0700

    LC - Binary Tree Paths

--- trees/cpp/binary-tree-paths.cpp
+++ trees/cpp/binary-tree-paths.cpp
@@ -14,32 +14,29 @@
  */
 
 class Solution {
-    vector<int> res_int;
-    vector<string> res;
-public:
-    vector<string> binaryTreePaths(TreeNode* root) {
-        if(!root) {
-            return res;
-        }
-        res_int.push_back(root->val); // push current root in int vector
-        if(root->left) {
-            binaryTreePaths(root->left); // visit left subtree
-        }
-        if(root->right) {
-            binaryTreePaths(root->right);  // visit right subtree
-        }
-        if(!root->left && !root->right) { // visit leaf node
-            string temp;
-            for(int i = 0; i < res_int.size(); i++) {
-                if(i != res_int.size() - 1) {
-                    temp += to_string(res_int[i]) + "->";
-                } else {
-                    temp += to_string(res_int[i]);
-                }
-            }
-            res.push_back(temp); // add string to result vector; path ended
-        }
-        res_int.pop_back(); // pop the visited node from int vector
-        return res;
-    }
-};
+     vector<string> result;
+     string path;
+ public:
+     vector<string> binaryTreePaths(TreeNode* root) {
+         if(!root) {
+             return result;
+         }
+         getPaths(root, path);
+         return result;
+     }
+
+     void getPaths(TreeNode* root, string path) {
+         if(!root) {
+             return;
+         }
+         if(!root->left && !root->right) {
+             path += to_string(root->val); // leaf node
+             result.push_back(path);
+         } else {
+             path += to_string(root->val)  + "->";
+         }
+         getPaths(root->left, path);
+         getPaths(root->right, path);
+         return;
+     }
+ };
